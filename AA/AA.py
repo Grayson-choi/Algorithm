321bbd23f677a2757b6564cbc654d5c77839c7af
Grayson-choi <784wldnd@gmail.com>
2021-03-08 19:47:56 +0900

    문제 풀이 추가

--- AA/AA.py
+++ AA/AA.py
@@ -1,34 +1,43 @@
 import sys
-import itertools
-from pprint import pprint
 
 """
-6 0
-60 60 90 60 60 60
-"""
+5 20
+10 5
+25 12
+15 8
+6 3
+7 4
 
 """
-5
-"""
 
-import sys
-sys.stdin=open("input.txt", "r")
-from collections import deque
-
-n, m = map(int, input().split())
-Q = [(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
-Q = deque(Q)
-cnt = 0
-while True:
-    cur = Q.popleft()
-    if any(cur[1] < x[1] for x in Q):
-        Q.append(cur)
-    else:
-        cnt += 1
-        if cur[0] == m:
-            print(cnt)
-            break
+sys.stdin = open("input.txt", "r")
 
 
+def dfs(v):
+    if v > n:
+        sub_sums = []
+        for i in range(n):
+            if ck_list[i] == 1:
+                sub_sums.append(nums[i])
+        sub_sum = sum(sub_sums)
+        print(sub_sum)
+    print()
+
+    ck_list[v] = 1
+    dfs(v + 1)
+    ck_list[v] = 0
+    dfs(v + 1)
 
 
+if __name__ == "__main__":
+    n, time = map(int, input().split())
+    p_score = [0] * (n + 1)
+    p_time = [0] * (n + 1)
+
+    for i in range(n):
+        score, time = map(int, input().split())
+        
+    nums = list(map(int, input().split()))
+    ck_list = [0] * (n + 1)
+    sums = []
+    dfs(1)
