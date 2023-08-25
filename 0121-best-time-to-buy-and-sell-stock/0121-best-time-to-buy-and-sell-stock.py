78e1a8985e6308dd4450e711af527c9c2a47fe30
Grayson-choi <47515509+Grayson-choi@users.noreply.github.com>
2023-08-25 14:20:54 +0900

    Time: 762 ms (99.72%), Space: 27.3 MB (91.74%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf') # 최소 가격 초기화
+        max_profit = 0 # 최대 이익 초기화
+
+        for price in prices:
+            if price < min_price:
+                min_price = price # 최소 가격 업데이트
+            elif price - min_price > max_profit:
+                max_profit = price - min_price # 최대 이익 업데이트
+
+        return max_profit
+
